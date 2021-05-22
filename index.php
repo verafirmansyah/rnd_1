9aa6ff352603d4f0019a1baf155e025ae78c5471
Vera Firmansyah <38237073+verafirmansyah@users.noreply.github.com>
2021-05-22 10:00:29 +0700

    Commit pertama
    awal upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+	<title>Pelatihan hari pertama RND</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/sweetalert2@9.17.2/dist/sweetalert2.min.css">
+</head>
+<body>
+	<div class="judul">		
+		<h1>Membuat CRUD Dengan PHP Dan MySQL</h1>
+		<h2>Menampilkan data dari database</h2>
+		<h3>www.malasngoding.com</h3>
+	</div>
+	<br/>
+ 
+	<?php 
+	if(isset($_GET['pesan'])){
+		$pesan = $_GET['pesan'];
+		if($pesan == "input"){
+			echo "Data berhasil di input.";
+		}else if($pesan == "update"){
+			echo "Data berhasil di update.";
+		}else if($pesan == "hapus"){
+			echo "Data berhasil di hapus.";
+		}
+	}
+	?>
+	<br/>
+	<a class="tombol" href="input.php">+ Tambah Data Baru</a>
+ 
+	<h3>Data user</h3>
+	<table border="1" class="table">
+		<tr>
+			<th>No</th>
+			<th>Nama</th>
+			<th>Alamat</th>
+			<th>Pekerjaan</th>
+			<th>Opsi</th>		
+		</tr>
+		<?php 
+		include "koneksi.php";
+		$sql = "SELECT * FROM user";
+		$query_mysql = mysqli_query($host, $sql) or die(mysqli_error());
+		$nomor = 1;
+		while($data = mysqli_fetch_array($query_mysql)){
+		?>
+		<tr>
+			<td><?php echo $nomor++; ?></td>
+			<td><?php echo $data['nama']; ?></td>
+			<td><?php echo $data['alamat']; ?></td>
+			<td><?php echo $data['pekerjaan']; ?></td>
+			<td>
+				<a class="edit" href="edit.php?id=<?php echo $data['id']; ?>">Edit</a> |
+				<!-- <a class="hapus" href="hapus.php?id=<?php echo $data['id']; ?>">Hapus</a>					 -->
+				<a class="hapus" href="#" onclick="onHapus(<?php echo $data['id']; ?>)">Hapus</a>					
+				<a class="hapus" href="#" onclick="testAlert()">Alert</a>					
+			</td>
+		</tr>
+		<?php } ?>
+	</table>
+</body>
+</html>
+
+<script type="text/javascript">
+	
+function onHapus(id) {
+	// alert('hello on hapus' + id);
+	if (confirm('Anda yakin untuk menghapus id '+id)) {
+		document.location.href='hapus.php?id='+id;
+		alert(id+ ' telah terhapus');
+		document.location.href='index.php';
+	}
+}
+
+function testAlert() {
+	Swal.fire('Any fool can use a computer')
+}
+
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.17.2/dist/sweetalert2.all.min.js"></script>
---
+++ malasngoding.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2021 at 11:44 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `malasngoding`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `pekerjaan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `alamat`, `pekerjaan`) VALUES
+(1, 'Andi edit', 'Surabaya edit', 'web programmer edit'),
+(6, 'Samsul', 'Sumedang', 'Pegawai'),
+(64, 'Ujang', 'Cicendo', 'Guru'),
+(65, 'A', 'A', 'A'),
+(66, 'B1q', 'B1q', 'B1q');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
